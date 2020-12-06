6e44552c7c3f3659c480e2f1baff92d7b37d0de6
Jacek Kuśnierz <jaku@grapeup.com>
2020-12-06 03:39:49 +0100

    more updates

--- lambda.py
+++ lambda.py
@@ -8,14 +8,22 @@ def lambda_handler(event, context):
     print("started")
     s3 = boto3.client('s3')
     print("downloading obj")
-    s3.download_file(event['in_bucket_name'], event['file_path'], '/tmp/in.root')
+    try:
+        os.mkdir('/mnt/cern_root/tmp')
+    except OSError as error:
+        print(error)
+
+        # s3.download_file(event['in_bucket_name'], event['file_path'], '/mnt/cern_root/tmp/in.root')
     print("streamed obj")
 
     intro = """
 import sys
 print(sys.argv)
-fileName = sys.argv[0]
-treeName = sys.argv[1]
+import ROOT
+import PyRDF
+
+fileName = sys.argv[1]
+treeName = sys.argv[2]
 """
 
     outro = """
@@ -25,7 +33,7 @@ hist.write()
 outHistFile.Close()
 """
 
-    script_file = open('/tmp/script.py', "a")
+    script_file = open('/mnt/cern_root/root_install/PyRDF/script.py', "w")
     script_file.write(intro)
     script_file.write(event['script'])
     script_file.write(outro)
@@ -34,12 +42,12 @@ outHistFile.Close()
     result = os.system('''
         export PATH=/mnt/cern_root/chroot/usr/local/sbin:/mnt/cern_root/chroot/usr/local/bin:/mnt/cern_root/chroot/usr/sbin:/mnt/cern_root/chroot/usr/bin:/mnt/cern_root/chroot/sbin:/mnt/cern_root/chroot/bin:$PATH && \
         export LD_LIBRARY_PATH=/mnt/cern_root/chroot/usr/lib64:/mnt/cern_root/chroot/usr/lib:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH && \
-        export CPATH=/mnt/cern_root/chroot/usr/include:$CPATH && \      
+        export CPATH=/mnt/cern_root/chroot/usr/include:$CPATH && \
+        export PYTHONPATH=/mnt/cern_root/root_install/PyRDF:/mnt/cern_root/root_install:$PYTHONPATH && \
         export roothome=/mnt/cern_root/root_install && \
-        export PYTHONPATH=/mnt/cern_root/root_install/PyRDF:$PYTHONPATH && \     
+        cd /mnt/cern_root/root_install/PyRDF && \
         . ${roothome}/bin/thisroot.sh && \
-        cd /tmp && \
-        python3.7 /tmp/script.py /tmp/in.root myTree
+        /mnt/cern_root/chroot/usr/bin/python3.7 /mnt/cern_root/root_install/PyRDF/script.py /mnt/cern_root/tmp/in.root myTree
     ''')
     if not result:
         return {
