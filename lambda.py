ea022eebb827252dd834f38416bbe5f09e9d774a
Jacek Kuśnierz <jaku@grapeup.com>
2021-03-06 18:30:15 +0100

    load all to range

--- lambda.py
+++ lambda.py
@@ -13,13 +13,23 @@ def lambda_handler(event, context):
 
     start = str(event['start'])
     end = str(event['end'])
+    filelist= str(event['filelist'])
+    friend_info=  event.get('friend_info')
 
     range = base64.b64decode(event['range'][2:-1])
     mapper = base64.b64decode(event['script'][2:-1])
+
     print("streamed obj")
 
     mapper=pickle.loads(mapper)
     range=pickle.loads(range)
+
+    range.start=start
+    range.end=end
+    range.filelist=filelist
+    if friend_info:
+        range.friend_info=pickle.loads(base64.b64decode(friend_info)[2:-1])
+
     hist=mapper(range)
     pickle.dump(hist, open('/tmp/out.pickle','wb'))
 
