0c66ac8759cfa3dba2d5b0a86045350545f6c338
Jacek Kuśnierz <jaku@grapeup.com>
2021-03-06 15:49:12 +0100

    docker init?

--- lambda.py
+++ lambda.py
@@ -1,11 +1,11 @@
 import base64
 import json
 import os
+import pickle
 
 import boto3
 
 bucket = os.environ.get('bucket')
-
 
 def lambda_handler(event, context):
     print('event', event)
@@ -18,37 +18,29 @@ def lambda_handler(event, context):
     mapper = base64.b64decode(event['script'][2:-1])
     print("streamed obj")
 
-    glue = f"""
-import pickle
-mapper=pickle.loads({mapper})
-range=pickle.loads({range})
-hist=mapper(range)
-pickle.dump(hist, open('/tmp/out.pickle','wb'))
-"""
+    mapper=pickle.loads({mapper})
+    range=pickle.loads({range})
+    hist=mapper(range)
+    pickle.dump(hist, open('/tmp/out.pickle','wb'))
 
-    script_file = open('/tmp/to_execute.py', "w")
-    script_file.write(glue)
-    script_file.close()
-
-    result = os.system('''
-        export PATH=/mnt/cern_root/chroot/usr/local/sbin:/mnt/cern_root/chroot/usr/local/bin:/mnt/cern_root/chroot/usr/sbin:/mnt/cern_root/chroot/usr/bin:/mnt/cern_root/chroot/sbin:/mnt/cern_root/chroot/bin:$PATH && \
-        export LD_LIBRARY_PATH=/mnt/cern_root/chroot/usr/lib64:/mnt/cern_root/chroot/usr/lib:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH && \
-        export CPATH=/mnt/cern_root/chroot/usr/include:$CPATH && \
-        export PYTHONPATH=/mnt/cern_root/root_install/PyRDF:/mnt/cern_root/root_install:$PYTHONPATH && \
-        export roothome=/mnt/cern_root/root_install && \
-        cd /mnt/cern_root/root_install/PyRDF && \
-        . ${roothome}/bin/thisroot.sh && \
-        /mnt/cern_root/chroot/usr/bin/python3.7 /tmp/to_execute.py
-    ''')
+    # result = os.system('''
+    #     export PATH=/mnt/cern_root/chroot/usr/local/sbin:/mnt/cern_root/chroot/usr/local/bin:/mnt/cern_root/chroot/usr/sbin:/mnt/cern_root/chroot/usr/bin:/mnt/cern_root/chroot/sbin:/mnt/cern_root/chroot/bin:$PATH && \
+    #     export LD_LIBRARY_PATH=/mnt/cern_root/chroot/usr/lib64:/mnt/cern_root/chroot/usr/lib:/usr/lib64:/usr/lib:$LD_LIBRARY_PATH && \
+    #     export CPATH=/mnt/cern_root/chroot/usr/include:$CPATH && \
+    #     export PYTHONPATH=/mnt/cern_root/root_install/PyRDF:/mnt/cern_root/root_install:$PYTHONPATH && \
+    #     export roothome=/mnt/cern_root/root_install && \
+    #     cd /mnt/cern_root/root_install/PyRDF && \
+    #     . ${roothome}/bin/thisroot.sh && \
+    #     /mnt/cern_root/chroot/usr/bin/python3.7 /tmp/to_execute.py
+    # ''')
     s3.upload_file(f'/tmp/out.pickle', bucket, f'partial_{start}_{end}.pickle')
 
-    if not result:
-        return {
-            'statusCode': 500,
-            'body': json.dumps('Failed!'),
-            'result': json.dumps(result)
-        }
-
+    # if not result:
+    #     return {
+    #         'statusCode': 500,
+    #         'body': json.dumps('Failed!'),
+    #         'result': json.dumps(result)
+    #     }
 
     return {
         'statusCode': 200,
